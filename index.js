de85734d9a50f93dc0d472c2ba726f0929f04481
crt-romain78 <75182534+crt-romain78@users.noreply.github.com>
2021-05-25 09:19:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ pid: process.pid,
 activity : {
 details : "Cosmos bot list 2021",
 assets : {
-large_image : "discord-avatar-512-6kz75",
+large_image : "",//picture it will show on your status
 large_text : "Cosmos bot list" // THIS WILL SHOW AS "Playing <Status>" from the outisde
 },
 buttons : [{label : "Bot-list" , url : "https://cosmos-botlist.ml"},{label : "Support Server",url : "https://dsc.gg/cosmos-suport"}]
 }
 })
 })
-client.login({ clientId : "846642614270820354" }).catch(console.error);
+client.login({ clientId : "YourApplicationId" }).catch(console.error);
